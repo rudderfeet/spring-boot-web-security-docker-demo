88acb2ccca80fd468f22c75b53f7bd4872db1c97
Scott McCrory <scott@mccrory.us>
2018-04-01 13:08:05 -0400

    Added project docs

---
+++ src/main/java/us/mccrory/springbootwebsecuritydockerdemo/SpringBootWebSecurityDockerDemoApplication.java
@@ -0,0 +1,12 @@
+package us.mccrory.springbootwebsecuritydockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSecurityDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSecurityDockerDemoApplication.class, args);
+	}
+}
